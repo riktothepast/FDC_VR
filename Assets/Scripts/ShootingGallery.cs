84bfddeb0a127e700a4271d75167cabf062d18dd
riktothepast <rik@502studios.net>
2018-10-26 21:37:00 -0600

    Adds base VR project

---
+++ Assets/Scripts/ShootingGallery.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ShootingGallery : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject ghostPrefab;
+    [SerializeField]
+    protected float spawnInterval = 2f;
+
+    public void StartGame () {
+        InvokeRepeating("CreateGhost", spawnInterval, spawnInterval);
+	}
+
+    public void StopGame() {
+        CancelInvoke();
+    }
+
+    public void CreateGhost() {
+        Vector3 direction = Vector3.up;
+        direction.x = Random.Range(-0.5f, 0.5f);
+
+        Vector3 position = transform.position;
+        position.x += Random.Range(-4, 4);
+
+        Target ghost = Instantiate(ghostPrefab).GetComponent<Target>();
+        ghost.transform.position = position;
+        ghost.AssignDirection(direction);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    [SerializeField]
+    protected float moveSpeed;
+    [SerializeField]
+    protected float TTL;
+    Vector3 direction;
+
+    private void Start()
+    {
+        Invoke("RemoveEntity", TTL);
+    }
+
+    void Update () {
+        transform.localScale = new Vector3(1 + Mathf.PingPong(Time.time, 0.2f), 1 + Mathf.PingPong(Time.time, 0.2f), 1 + Mathf.PingPong(Time.time, 0.2f));
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+	}
+
+    public void AssignDirection(Vector3 dir) {
+        direction = dir;
+    }
+
+    public void Hit()
+    {
+        RemoveEntity();
+    }
+
+    void RemoveEntity()
+    {
+        Destroy(gameObject);
+    }
+}
